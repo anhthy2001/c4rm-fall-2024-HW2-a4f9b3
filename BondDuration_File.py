c024b33e4bd8f0d449a47f0ad2126ba9152f4c1a
anhthy2001 <tn2551@columbia.edu>
2025-02-25 09:54:02 -0500

    BondDuration_File.py
    1

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -7,13 +7,10 @@ def getBondPrice(y, face, couponRate, m, ppy=1):
     t = np.arange(1, N + 1)  
 
     pv_coupons = np.sum(couponPayment / (1 + period_ytm) ** t)
-
     pv_face_value = face / (1 + period_ytm) ** N
-
     bond_price = pv_coupons + pv_face_value
     
     return bond_price  
-
 
 def getBondDuration(y, face, couponRate, m, ppy):
     bondValue = getBondPrice(y, face, couponRate, m, ppy)
@@ -22,10 +19,8 @@ def getBondDuration(y, face, couponRate, m, ppy):
     period_ytm = y / ppy
     t = np.arange(1, periods + 1)
 
-
-    weightedPV = np.sum((t * couponPayment) / ((1 + period_ytm) ** t))
-    weightedPV_face_value = (periods * face) / ((1 + period_ytm) ** periods)
-    weightedPV = weightedPV + weightedPV_face_value
+    weightedPV = np.sum((t * couponPayment) / ((1 + period_ytm) ** t)) 
+    weightedPV += (periods * face) / ((1 + period_ytm) ** periods)  
 
     bondDuration = weightedPV / bondValue
     return bondDuration
@@ -34,10 +29,9 @@ y = 0.03
 couponRate = 0.04
 face = 2000000
 m = 10
-ppy = 1
 
+Duration_annual = getBondDuration(y, face, couponRate, m, ppy=1)
+print(f"Bond Duration (Annual): {Duration_annual:.2f}")  
 
-Duration = getBondDuration(y, face, couponRate, m, ppy)
-
-
-print(f"Bond Duration: {Duration:.2f}")
+Duration_semiannual = getBondDuration(y, face, couponRate, m, ppy=2)
+print(f"Bond Duration (Semiannual): {Duration_semiannual:.2f}") 
